ec245a7e54c911000842b408cd5046d1100a10f0
s1uggard <alexeygeno@gmail.com>
2023-09-14 22:43:26 +0300

    composer require webapp

--- tests/Application/config/routes/alex_geno_phone_verification.php
+++ tests/Application/config/routes/alex_geno_phone_verification.php
@@ -2,5 +2,5 @@
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes): void {
-    $routes->import('@AlexGenoPhoneVerificationBundle/Resources/config/routes/phone_verification.yaml');
+    $routes->import('@AlexGenoPhoneVerificationBundle/Resources/config/routes/alex_geno_phone_verification.yaml');
 };
